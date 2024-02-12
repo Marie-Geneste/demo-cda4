00c25db0df6bb9102d7fc605be0e73b2da481ef2
Marie-Geneste <marie.e.geneste@gmail.com>
2024-02-12 12:15:40 +0100

    add Form new produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +21,18 @@ class ProduitController extends AbstractController
         return $this->render('produit/index.html.twig', [
             //'controller_name' => 'ProduitController',
             'produits' => $produits,
+        ]);
+    }
+
+    #[Route('/produit/new', name: 'app_produit_new')]
+    public function new(): Response {
+
+        $produit = new Produit();
+
+        $form = $this-> createForm(ProduitType::class, $produit);
+
+        return $this->render('produit/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
